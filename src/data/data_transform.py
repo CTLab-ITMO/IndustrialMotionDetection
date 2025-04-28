18799c72d3e0b01fca1286b2a9e0ad164abe0c96
zarus03 <zaripovruslan864@gmail.com>
2025-04-28 14:47:13 +0300

    rename file

--- src/data/data_transform.py
+++ src/data/data_transform.py
@@ -1,4 +1,4 @@
-from transforms.transforms import (
+from transforms_impl.transforms import (
     Compose,
     Lambda,
     NormalizeVideo,
@@ -8,13 +8,12 @@ from transforms.transforms import (
 )
 
 
-clip_len = 16
-size = 224
-mean = [0.485, 0.456, 0.406]
-std = [0.229, 0.224, 0.225]
-pixel_max_value = 255.0
-
 def get_transform():
+    clip_len = 16
+    size = 224
+    mean = [0.485, 0.456, 0.406]
+    std = [0.229, 0.224, 0.225]
+    pixel_max_value = 255.0
     return Compose([
         UniformTemporalSubsample(clip_len),
         Lambda(lambda x: x/pixel_max_value),
